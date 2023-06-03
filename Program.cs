9ba0ce99e365cdc9acd26c95c0c285511514269c
Jeffrey <572199+darkthread@users.noreply.github.com>
2023-06-03 19:44:20 +0800

    yield return + Parallel.ForEach

--- Program.cs
+++ Program.cs
@@ -1,18 +1,11 @@
 ﻿using System.Diagnostics;
 
-int pwdLength = 3;
-string pwdCharSet = "*"; // N-Number, A-AlphaDigit, *-Any
-int minThreads = Environment.ProcessorCount * 4;
-int maxQueueItems = 65536;
+int pwdLength = 4;
+string pwdCharSet = "n"; // N-Number, A-AlphaDigit, *-Any
 if (args.Length > 0)
     int.TryParse(args[0], out pwdLength);
 if (args.Length > 1)
     pwdCharSet = args[1];
-if (args.Length > 2)
-    int.TryParse(args[2], out minThreads);
-if (args.Length > 3)
-    int.TryParse(args[3], out maxQueueItems);
-
 // 密碼字元範圍：ASCII 32~126
 char[] pwdChars;
 switch (pwdCharSet.ToUpper())
@@ -26,7 +19,7 @@ switch (pwdCharSet.ToUpper())
     default: // All
         pwdChars = Enumerable.Range(32, 95).Select(i => (char)i).ToArray();
         break;
-} 
+}
 
 // 原始資料：1KB byte[]
 var plain = new byte[1024];
@@ -45,30 +38,50 @@ Console.WriteLine($"AES加密內容: {BitConverter.ToString(ecn.data.Take(16).To
 Console.WriteLine($"AES-CBC IV: {BitConverter.ToString(ecn.iv)}");
 
 var cts = new CancellationTokenSource();
+var token = cts.Token;
 var sw = new Stopwatch();
 var startTime = DateTime.Now;
 long total = Enumerable.Range(1, pwdLength).Sum(i => Convert.ToInt64(Math.Pow(pwdChars.Length, i)));
-long queued = 0;
+long errCount = 0;
 long completed = 0;
-ThreadPool.SetMinThreads(minThreads, 16);
 Task.Run(() =>
     {
-
         // 檢查 CancellationTokenSource 是否已經被取消
         while (!cts.Token.IsCancellationRequested)
         {
             Console.CursorLeft = 0;
-            Console.Write($"{(DateTime.Now - startTime).TotalSeconds,3:n0}s | {ThreadPool.ThreadCount,7} | {ThreadPool.PendingWorkItemCount,7} | {completed:n0} / {total:n0} ({completed * 1.0 / total:p0})");
+            Console.Write($"{(DateTime.Now - startTime).TotalSeconds,3:n0}s | {completed:n0} / {total:n0} ({completed * 1.0 / total:p0}) | {errCount:n0}");
             Thread.Sleep(1000);
         }
-    }, 
+    },
     cts.Token //允許還沒執行前取消(例如：還在 Queue 排隊時就取消)
 );
 var tasks = new List<Task>();
 sw.Start();
-explore(string.Empty);
+Parallel.ForEach<string>(explore(""), new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount * 2 },
+(pwd) =>
+{
+    if (token.IsCancellationRequested)
+        return;
+    try
+    {
+        var dec = CodecNetFx.AesDecrypt(pwd, ecn.data, ecn.iv);
+        if (dec.SequenceEqual(plain))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($" ** 發現密碼：{pwd} **");
+            Console.ResetColor();
+        }
+    }
+    catch { 
+        Interlocked.Increment(ref errCount);
+    }
+    finally
+    {
+        Interlocked.Increment(ref completed);
+    }
+});
 
-Task.WaitAll(tasks.ToArray());
 Console.WriteLine();
 cts.Cancel();
 sw.Stop();
@@ -77,43 +90,16 @@ Console.ForegroundColor = ConsoleColor.Cyan;
 Console.WriteLine($"耗時: {sw.ElapsedMilliseconds:n0}ms");
 Console.ResetColor();
 
-
-// DFS 產生所有組合
-void explore(string pfx)
+IEnumerable<string> explore(string currPwd)
 {
-    var pwd = pfx;
-    if (!string.IsNullOrEmpty(pwd))
-    {
-        // 雖然 ThreadPool 待處理 Work Item 沒有上限，但為避免耗用過多資源，加上限制
-        while (ThreadPool.PendingWorkItemCount > maxQueueItems)
-        {
-            Thread.Sleep(1);
-        }
-        queued++;
-        tasks.Add(Task.Run(() =>
-        {
-            try
-            {
-                var dec = CodecNetFx.AesDecrypt(pwd, ecn.data, ecn.iv);
-                if (dec.SequenceEqual(plain))
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($" ** 發現密碼：{pwd} **");
-                    Console.ResetColor();
-                }
-            }
-            catch { }
-            finally
-            {
-                Interlocked.Increment(ref completed);
-            }
-        }));
-    }
-    if (pfx.Length < pwdLength)
+    if (!string.IsNullOrEmpty(currPwd))
+        yield return currPwd;
+    if (currPwd.Length < pwdLength)
     {
         foreach (var c in pwdChars)
         {
-            explore(pfx + c);
+            foreach (var res in explore(currPwd + c))
+                yield return res;
         }
     }
 }
